416f5ea05788262ebacc206197078e5cbed9afe1
Dangyi Liu <leedy.liu@gmail.com>
2016-12-15 16:31:18 -0800

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-daniel = User.find_or_create_by!(name: 'Daniel', email: 'daniel@ucsd.edu', password: '123456')
+dangyi = User.find_or_create_by!(name: 'Dangyi', email: 'dangyi@ucsd.edu', password: '123456')
+weichen = User.find_or_create_by!(name: 'Weichen', email: 'weichen@ucsd.edu', password: '123456')
+steven = User.find_or_create_by!(name: 'Steven', email: 'steven@ucsd.edu', password: '123456')
 
 decision = Decision.find_or_create_by!(name: 'FirstDecision', description: 'Our first decision', visibility: :private)
 
-Membership.find_or_create_by!(user: daniel, decision: decision, role: :owner)
+Membership.find_or_create_by!(user: dangyi, decision: decision, role: :owner)
+Membership.find_or_create_by!(user: weichen, decision: decision, role: :member)
+Membership.find_or_create_by!(user: steven, decision: decision, role: :member)
 
-%w[ James Mary Paul Linda Dave ].each do |e|
+Alternative.destroy_all
+Criterium.destroy_all
+Score.destroy_all
+
+%w[ James Mary Paul Linda ].each do |e|
   Alternative.create!(name: e, decision: decision)
 end
 
 ['Academic', 'Recommendation Letter', 'Readiness for Engineering'].each do |e|
   Criterium.create!(name: e, decision: decision)
+end
+
+Alternative.each do |a|
+  Criterium.each do |c|
+    Membership.each do |u|
+      Score.create(membership: u, value: rand(100)/10.0, alternative: a, criterium: c)
+    end
+  end
 end
